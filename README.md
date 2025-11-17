d184f83bf465629606bf7522c961e1222a876be1
Marcel Nguemaha <marcel.nguemaha@gmail.com>
2025-11-17 14:09:44 -0500

    v1

---
+++ README.md
@@ -0,0 +1,3 @@
+https://openai.com/news/
+https://www.anthropic.com/research
+https://www.anthropic.com/engineering
---
+++ app/config.py
@@ -0,0 +1,5 @@
+# Define youtube channels to scrape (by channel id)
+YOUTUBE_CHANNELS = [
+    # "UCn8ujwUInbJkBhffxqAPBVQ", # Dave Ebbelaar
+    "UCawZsQWqfGSbCI5yjkdVkTA", # Matthew Berman
+]
